3089485d5aa89618a4aa5064e42bcd6ceb2daf95
Lulu-wiszenka <76125783+Lulu-wiszenka@users.noreply.github.com>
2025-03-21 21:49:43 +0200

    Delete src directory

--- src/main/java/com/example/familyplanner/service/exception/InvalidJwtException.java
+++
@@ -1,9 +0,0 @@
-package com.example.familyplanner.service.exception;
-
-import org.springframework.security.core.AuthenticationException;
-
-public class InvalidJwtException extends AuthenticationException {
-    public InvalidJwtException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/example/familyplanner/service/exception/ValidationException.java
+++
@@ -1,7 +0,0 @@
-package com.example.familyplanner.service.exception;
-
-public class ValidationException extends RuntimeException {
-    public ValidationException(String message) {
-        super(message);
-    }
-}
